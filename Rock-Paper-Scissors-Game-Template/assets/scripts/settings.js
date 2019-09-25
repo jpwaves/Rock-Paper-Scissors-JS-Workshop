8a9192e0e3eaaf5e84dde0366014529f738145c8
Justin Pong <48561685+jpwaves@users.noreply.github.com>
2019-09-24 23:47:32 -0700

    Add files via upload
    Finished configuring the functionality of the settings popup. Needs code in script.js to implement functionality into the game.

--- Rock-Paper-Scissors-Game-Template/assets/scripts/settings.js
+++ Rock-Paper-Scissors-Game-Template/assets/scripts/settings.js
@@ -1,15 +1,13 @@
 function closeWindow() {
   document.getElementById("settingsOptions").style.display = "none";
   document.getElementById("blurredBackground").style.display = "none";
-  document.getElementById("background").style.backgroundImage = "linear-gradient(to bottom right, rgb(0, 255, 213), rgb(53, 117, 255));";
   document.getElementById("background").style.backgroundColor = "transparent";
-  //document.body.style.position = "static";
+  // document.body.style.position = "static";
 }
 
 function openWindow() {
   document.getElementById("settingsOptions").style.display = "initial";
-  document.getElementById("background").style.display = "initial";
-  document.getElementById("background").style.backgroundImage = "";
+  document.getElementById("blurredBackground").style.display = "initial";
   document.getElementById("background").style.backgroundColor = "rgba(255, 255, 255, 0.8)";
-  //document.body.style.position = "fixed";
+  // document.body.style.position = "fixed";
 }
