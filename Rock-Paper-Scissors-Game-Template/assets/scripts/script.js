a6c57b41345a4297a423ed87565b8e1f08d7c87b
Justin Pong <48561685+jpwaves@users.noreply.github.com>
2019-09-25 12:39:14 -0700

    - editted resetBoard()

--- Rock-Paper-Scissors-Game-Template/assets/scripts/script.js
+++ Rock-Paper-Scissors-Game-Template/assets/scripts/script.js
@@ -1,8 +1,15 @@
 let typeOfImages = "hand";
 const imgPath = "assets/images/"
+let isGameActive = false;
 
-function changeImages(imgType) {
+function changeImgAndText(entity, move) {
+  if (typeOfImages == "hand") {
+    document.getElementById(`${entity}Image`).src = `${imgPath}${move}-hand-emoji.png`;
+  } else {
+    document.getElementById(`${entity}Image`).src = `${imgPath}${move}-obj-emoji.png`;
   
+  }
+  document.getElementById(`${entity}ImageLabel`).innerHTML = move.substring(0, 1).toUpperCase() + move.substring(1, move.length);
 }
 
 function applySettings() {
@@ -12,6 +19,11 @@ function applySettings() {
     typeOfImages = "hand";
   } else {
     typeOfImages = "literal"
+  }
+  console.log(`Image Settings: ${typeOfImages.toUpperCase()}`);
+  if (isGameActive) {
+    changeImgAndText("player", document.getElementById("playerImageLabel").innerHTML.toLowerCase().trim());
+    changeImgAndText("computer", document.getElementById("computerImageLabel").innerHTML.toLowerCase().trim());
   }
 }
 
@@ -24,6 +36,7 @@ function loadSettingsButtons() {
 }
 
 function getComputerTurn() {
+  isGameActive = true;
   const probability = Math.floor(Math.random() * 100);
   console.log(`Computer probability: ${probability}`);
   if (probability < 33) {
@@ -32,14 +45,6 @@ function getComputerTurn() {
     return "paper";
   } else {
     return "scissors";
-  }
-}
-
-function changeImg(entity, move) {
-  if (typeOfImages == "hand") {
-    document.getElementById(`${entity}Image`).src = `${imgPath}${move}-hand-emoji.png`;
-  } else {
-    document.getElementById(`${entity}Image`).src = `${imgPath}${move}-obj-emoji.png`;
   }
 }
 
@@ -68,15 +73,23 @@ function lose() {
   document.getElementById("result").innerHTML = "Computer wins this round!";
 }
 
-function reset() {
-  document.getElementById("playerScore") = "0";
-  document.getElementById("computerScore") = "0";
-  document.getElementById("result") = "";
-
+function resetBoard() {
+  console.log("Resetting...");
+  isGameActive = false;
+  document.getElementById("playerScore").innerHTML = "0";
+  document.getElementById("computerScore").innerHTML = "0";
+  document.getElementById("result").innerHTML = "";
+  console.log("Resetting... 33%");
+  document.getElementById("resetBtn").style.visibility = "hidden";
   document.getElementById("rock").disabled = false;
   document.getElementById("paper").disabled = false;
   document.getElementById("scissors").disabled = false;
-  document.getElementById("resetBtn").style.visibility = "visible";
+  console.log("Resetting... 66%");
+  document.getElementById("playerImage").src = "assets/images/player-head.png";
+  document.getElementById("computerImage").src = "assets/images/computer-head.png";
+  document.getElementById("playerImageLabel").innerHTML = "";
+  document.getElementById("computerImageLabel").innerHTML = "";
+  console.log("Reset Complete");
 }
 
 function initGameEndMessage(playerScore, computerScore) {
@@ -94,71 +107,72 @@ function initGameEndMessage(playerScore, computerScore) {
 }
 
 function isGameDone() {
-  const playerScore = document.getElementById("playerScore");
-  const computerScore = document.getElementById("computerScore");
+  const playerScore = parseInt(document.getElementById("playerScore").innerHTML);
+  const computerScore = parseInt(document.getElementById("computerScore").innerHTML);
   if (playerScore >= 25 || computerScore >= 25) {
     initGameEndMessage(playerScore, computerScore);
   }
+  console.log(`Checked if game is done. Score -> [ ${playerScore} : ${computerScore} ]`);
 }
 
 function chooseRock() {
-  changeImg("player", "rock");
+  changeImgAndText("player", "rock");
   const computerMove = getComputerTurn();
   if (computerMove == "scissors") {
     // Condition for a win
-    changeImg("computer", "scissors");
+    changeImgAndText("computer", "scissors");
     win();
     incrementPlayerScore();
   } else if (computerMove == "paper") {
     // Condition for a loss
-    changeImg("computer", "paper");
+    changeImgAndText("computer", "paper");
     lose();
     incrementComputerScore();
   } else {
     // Condition for tie
-    changeImg("computer", "rock");
+    changeImgAndText("computer", "rock");
     tie();
   }
   isGameDone();
 }
 
 function choosePaper() {
-  changeImg("player", "paper");
+  changeImgAndText("player", "paper");
   const computerMove = getComputerTurn();
   if (computerMove == "rock") {
     // Condition for a win
-    changeImg("computer", "rock");
+    changeImgAndText("computer", "rock");
     win();
     incrementPlayerScore();
   } else if (computerMove == "scissors") {
     // Condition for a loss
-    changeImg("computer", "scissors");
+    changeImgAndText("computer", "scissors");
     lose();
     incrementComputerScore();
   } else {
     // Condition for tie
-    changeImg("computer", "paper");
+    changeImgAndText("computer", "paper");
     tie();
   }
   isGameDone();
 }
 
 function chooseScissors() {
-  changeImg("player", "scissors");
+  changeImgAndText("player", "scissors");
   const computerMove = getComputerTurn();
   if (computerMove == "paper") {
     // Condition for a win
-    changeImg("computer", "paper");
+    changeImgAndText("computer", "paper");
     win();
     incrementPlayerScore();
   } else if (computerMove == "rock") {
     // Condition for a loss
-    changeImg("computer", "rock");
+    changeImgAndText("computer", "rock");
     lose();
     incrementComputerScore();
   } else {
     // Condition for tie
-    changeImg("computer", "scissors");
+    changeImgAndText("computer", "scissors");
     tie();
   }
   isGameDone();
