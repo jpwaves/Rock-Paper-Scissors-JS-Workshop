aae9637ba76cd76ae786a61816104b97066fff0d
Justin Pong <48561685+jpwaves@users.noreply.github.com>
2019-09-25 15:16:26 -0700

    Add files via upload
    - categorized functions into 2 groups: ones that participants will create and ones that will be premade.
    - order global vars and preloaded funcs alphabetically
    - added commits denoting above categorizations, stating steps completed by functions (essentially a step by step plan of what the functions they will build will do), and clearly indicating where participants will build their code.

--- Rock-Paper-Scissors-Game-Template/assets/scripts/script.js
+++ Rock-Paper-Scissors-Game-Template/assets/scripts/script.js
@@ -1,17 +1,10 @@
-let typeOfImages = "hand";
+// Global Variables
 const imgPath = "assets/images/"
 let isGameActive = false;
+let typeOfImages = "hand";
 
-function changeImgAndText(entity, move) {
-  if (typeOfImages == "hand") {
-    document.getElementById(`${entity}Image`).src = `${imgPath}${move}-hand-emoji.png`;
-  } else {
-    document.getElementById(`${entity}Image`).src = `${imgPath}${move}-obj-emoji.png`;
-  
-  }
-  document.getElementById(`${entity}ImageLabel`).innerHTML = move.substring(0, 1).toUpperCase() + move.substring(1, move.length);
-}
-
+// Essential functions for website functionality
+// DO NOT EDIT THESE!!!
 function applySettings() {
   var optionHand = document.getElementById("radio-hands");
   var optionLiteral = document.getElementById("radio-literal");
@@ -27,12 +20,13 @@ function applySettings() {
   }
 }
 
-function loadSettingsButtons() {
+function changeImgAndText(entity, move) {
   if (typeOfImages == "hand") {
-    document.getElementById("radio-hands").checked = "checked";
+    document.getElementById(`${entity}Image`).src = `${imgPath}${move}-hand-emoji.png`;
   } else {
-    document.getElementById("radio-literal").checked = "checked";
+    document.getElementById(`${entity}Image`).src = `${imgPath}${move}-obj-emoji.png`;
   }
+  document.getElementById(`${entity}ImageLabel`).innerHTML = move.substring(0, 1).toUpperCase() + move.substring(1, move.length);
 }
 
 function getComputerTurn() {
@@ -48,19 +42,41 @@ function getComputerTurn() {
   }
 }
 
-function incrementPlayerScore() {
-  let playerScore = parseInt(document.getElementById("playerScore").innerHTML);
-  document.getElementById("playerScore").innerHTML = `${playerScore + 1}`;
-}
-
 function incrementComputerScore() {
   let computerScore = parseInt(document.getElementById("computerScore").innerHTML);
   document.getElementById("computerScore").innerHTML = `${computerScore + 1}`;
 }
 
-function win() {
-  document.getElementById("result").style.color = "rgb(32, 255, 32)";
-  document.getElementById("result").innerHTML = "Player wins this round!";
+function incrementPlayerScore() {
+  let playerScore = parseInt(document.getElementById("playerScore").innerHTML);
+  document.getElementById("playerScore").innerHTML = `${playerScore + 1}`;
+}
+
+function initGameEndMessage(playerScore, computerScore) {
+  if (playerScore >= 25) {
+    win();
+    document.getElementById("result").innerHTML = "Player wins the game!";
+  } else {
+    lose();
+    document.getElementById("result").innerHTML = "Computer wins the game... :(";
+  }
+  document.getElementById("rock").disabled = true;
+  document.getElementById("paper").disabled = true;
+  document.getElementById("scissors").disabled = true;
+  document.getElementById("resetBtn").style.visibility = "visible";
+}
+
+function loadSettingsButtons() {
+  if (typeOfImages == "hand") {
+    document.getElementById("radio-hands").checked = "checked";
+  } else {
+    document.getElementById("radio-literal").checked = "checked";
+  }
+}
+
+function lose() {
+  document.getElementById("result").style.color = "red";
+  document.getElementById("result").innerHTML = "Computer wins this round!";
 }
 
 function tie() {
@@ -68,9 +84,9 @@ function tie() {
   document.getElementById("result").innerHTML = "This round is a tie!";
 }
 
-function lose() {
-  document.getElementById("result").style.color = "red";
-  document.getElementById("result").innerHTML = "Computer wins this round!";
+function win() {
+  document.getElementById("result").style.color = "rgb(32, 255, 32)";
+  document.getElementById("result").innerHTML = "Player wins this round!";
 }
 
 function resetBoard() {
@@ -105,15 +121,19 @@ function initGameEndMessage(playerScore, computerScore) {
   document.getElementById("scissors").disabled = true;
   document.getElementById("resetBtn").style.visibility = "visible";
 }
+/////////////////////////////////////////////
+// Your will be working on the code below. //
+/////////////////////////////////////////////
 
-function isGameDone() {
-  const playerScore = parseInt(document.getElementById("playerScore").innerHTML);
-  const computerScore = parseInt(document.getElementById("computerScore").innerHTML);
-  if (playerScore >= 25 || computerScore >= 25) {
-    initGameEndMessage(playerScore, computerScore);
-  }
-  console.log(`Checked if game is done. Score -> [ ${playerScore} : ${computerScore} ]`);
-}
+/*
+  Steps:
+    1. Generate computer's choice for the current round.
+    2. Compare the player's choice and the computer's choice to determine if the outcome is a win, tie, or loss for the player.
+    3. Depending on the player and computer's choices, change their corresponding pictures and labels appropriately, and change the scoreboard, if necessary.
+    4. Check if the game has reached the point threshold to stop the game and declare a winner.
+      Note: If there is a winner, execute some code that indicates who the winner is.
+    5. Repeat this for each of the functions below: chooseRock, choosePaper, and chooseScissors.
+*/
 
 function chooseRock() {
   changeImgAndText("player", "rock");
@@ -176,4 +196,13 @@ function chooseScissors() {
     tie();
   }
   isGameDone();
+}
+
+function isGameDone() {
+  const playerScore = parseInt(document.getElementById("playerScore").innerHTML);
+  const computerScore = parseInt(document.getElementById("computerScore").innerHTML);
+  if (playerScore >= 25 || computerScore >= 25) {
+    initGameEndMessage(playerScore, computerScore);
+  }
+  console.log(`Checked if game is done. Score -> [ ${playerScore} : ${computerScore} ]`);
 }
